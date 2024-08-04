1288734d81809cf032afd95b52d77ecc9c3c2036
Alla Varavina <fistawka27@gmail.com>
2024-08-04 23:08:38 +0200

    new

---
+++ cypress/e2e/POMtesty/inventory.cy.js
@@ -0,0 +1,47 @@
+/// <reference types="cypress" />
+
+import LoginPage from '../../support/pages/LoginPage';
+import testData from '../../fixtures/credentials.json';
+import InventoryPage from '../../support/pages/InventoryPage';
+
+describe('Login tests with POM', () => {
+  beforeEach(() => {
+    LoginPage.open();
+    LoginPage.loginWithCorrectData();
+  });
+
+  it('Check burger menu is visible', () => {
+    InventoryPage.burgerMenu.should('be.visible');
+    InventoryPage.burgerMenu.should('exist');
+  });
+
+  it('Check shopping card is visible', () => {
+    InventoryPage.shoppingCart.should('be.visible');
+    InventoryPage.shoppingCart.should('exist');
+  });
+
+  it('Check product sort icon is visible', () => {
+    InventoryPage.productSort.should('be.visible');
+    InventoryPage.productSort.should('exist');
+  });
+
+  it('Check product Sort list should have 4 options', () => {
+    InventoryPage.poductSortOptions().should('have.length', 4);
+  });
+
+  it('Check user redirect to CartPage when clicking on Shopping cart icon', () => {
+    InventoryPage.clickShoppingCart();
+    cy.url().should('eq', 'https://www.saucedemo.com/cart.html');
+  });
+
+  it('Check user redirect to ItemPage when clicking on Item', () => {
+    InventoryPage.clickProductItem();
+    cy.readFile('../../fixtures/savedNumber.json').then((data) => {
+      const idNumber = data.idNumber;
+      cy.url().should(
+        'eq',
+        `https://www.saucedemo.com/inventory-item.html?id=${idNumber}`
+      );
+    });
+  });
+});
---
+++ cypress/support/pages/InventoryPage.js
@@ -0,0 +1,42 @@
+/// <reference types="cypress" />
+
+import BasePage from './BasePage';
+class InventoryPage extends BasePage {
+  get burgerMenu() {
+    return cy.get('#react-burger-menu-btn');
+  }
+
+  get shoppingCart() {
+    return cy.get('[data-test="shopping-cart-link"]');
+  }
+
+  clickShoppingCart() {
+    return this.shoppingCart.click();
+  }
+
+  get productSort() {
+    return cy.get('[data-test="product-sort-container"]');
+  }
+
+  poductSortOptions() {
+    return this.productSort.find('option');
+  }
+
+  clickProductItem() {
+    cy.get('[data-test="inventory-item-name"]').then(($items) => {
+      const randomIndex = Math.floor(Math.random() * $items.length);
+      const $randomItem = $items.eq(randomIndex);
+      cy.wrap($randomItem)
+        .parent('a')
+        .invoke('attr', 'id')
+        .then((id) => {
+          const idNumber = id.match(/\d+/)[0];
+          cy.log('Extracted ID Number:', idNumber);
+          cy.writeFile('../../fixtures/savedNumber.json', { idNumber });
+          cy.wrap($randomItem).click();
+        });
+    });
+  }
+}
+
+export default new InventoryPage();
